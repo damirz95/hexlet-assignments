97706f6bfb3760183067eda1d277933e19dd8587
@hexlet/cli <support@hexlet.io>
2024-01-20 23:12:43 +0500

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -9,11 +9,15 @@ class App {
     public static boolean scrabble(List<String> words) {
         String[] word = words.get(0).toLowerCase().split("");
         String[] word2 = words.get(1).toLowerCase().split("");
-        int count = Math.max(words.get(0).length(), words.get(1).length());
+        int count = Math.min(words.get(0).length(), words.get(1).length());
+        List<String> wordList = Arrays.asList(word);
+        List<String> wordList2 = Arrays.asList(word2);
         for (int i = 0; i < count; i++) {
-            if (word[i].equals(word2[i])) {
+            if (wordList2.contains(wordList.get(i))) {
                 continue;
-            } else {return false;}
+            } else {
+                return false;
+            }
         }
         return true;
     }
